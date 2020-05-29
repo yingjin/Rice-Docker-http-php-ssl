ee4389b903c2399d282d5978559e80f79411ecf1
Yingjin <yj4@Yings-MacBook-Pro.local>
2020-05-28 22:05:48 -0500

    update

---
+++ inc.sh
@@ -0,0 +1,1 @@
+docker exec -it ying-http-ssl-php /bin/bash
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-docker run --name=ying-http-ssl-php -i -t -d -p 10443:10443 ying-http
+docker run --name=ying-http-ssl-php -i -t -d -p 443:443 ying-http
+docker network connect d6_dspacenet  ying-http-ssl-php
