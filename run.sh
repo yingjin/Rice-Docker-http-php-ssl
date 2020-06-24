3f71b40224459ab48a66b5cb1650289c4109cfe7
Yingjin <yj4@Yings-MacBook-Pro.local>
2020-06-23 22:50:07 -0500

    update scripts

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-docker run --name=ying-http-ssl-php -i -t -d -p 443:443 ying-http
+docker run --name=ying-http-ssl-php -i -t -d -p 443:443 \
+  -v /mnt/isilon/fondren-dss/dspace-test/test_assetstore:/var/www/html/assetstore \
+  -v /mnt/isilon/fondren-dss/dspace-test/streaming:/var/www/html/streaming ying-http
 docker network connect d6_dspacenet  ying-http-ssl-php
